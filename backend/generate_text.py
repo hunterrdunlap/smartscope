a167169f34c753a009549c95737eaaed07f6a61e
Hunter Dunlap <hunterdunlap@ymail.com>
2023-04-01 19:35:29 +0200

    feat: generate project layout

---
+++ backend/generate_text.py
@@ -0,0 +1,14 @@
+from main import Prompt
+import openai
+
+def generate_text(prompt: Prompt, max_tokens=100):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        temperature=1,
+    )
+    return response.choices[0].text.strip()
+
