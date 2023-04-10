c87e2dc7f80c7d5eb4284950c884bf7059cce3b9
Hunter Dunlap <hunterdunlap@ymail.com>
2023-04-10 20:36:17 +0200

    feat: add notion ingest and answering

--- backend/generate_text.py
+++ backend/generate_text.py
@@ -1,14 +1,11 @@
-from main import Prompt
-import openai
+from langchain.llms import OpenAI
+from langchain.chains import VectorDBQAWithSourcesChain
 
-def generate_text(prompt: Prompt, max_tokens=4000):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=1,
-    )
-    return response.choices[0].text.strip()
+
+def generate_text(text: str, api_key: str, model: str, store):
+    llm = OpenAI(model_name= model, temperature=0, openai_api_key=api_key)
+    chain = VectorDBQAWithSourcesChain.from_llm(llm=llm, vectorstore=store)
+    result = chain({"question": text})
+    result_text = result["answer"] + "\n\n" + result["sources"]
+    return result_text
 
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,24 @@
+import pickle
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from prompt import Prompt
 from generate_text import generate_text
-import os
+from os import environ as env
 from dotenv import load_dotenv
-import openai
+from prompt import Prompt
+import faiss
+from tiktoken import encoding_for_model
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+e = env.get
 
 app = FastAPI()
+
+# set up our index for fast retrieval
+index = faiss.read_index("docs.index")
+with open("faiss_store.pkl", "rb") as f:
+    store = pickle.load(f)
+    
+store.index = index
 
 @app.get("/")
 def read_root():
@@ -18,10 +27,17 @@ def read_root():
 @app.post("/generate")
 def generate_chat(prompt: Prompt):
     try:
-        generated_text = generate_text(prompt.text)
+        generated_text = generate_text(prompt.text, api_key = e("OPENAI_API_KEY"), model = e("MODEL"), store=store)
         return {"response": generated_text}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=str(ex))
+    
+
+# helper function for counting tokens
+def count_tokens(text: str) -> int:
+    enc = encoding_for_model(e("MODEL"))
+    tokens = enc(text)
+    return len(tokens)
     
     
 app.add_middleware(
